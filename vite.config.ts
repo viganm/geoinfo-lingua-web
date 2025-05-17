d847209b21a9bcdf1e2a9a436af0445f6dd3422a
vigan1 <vigan.mustafa.vm@gmail.com>
2025-05-17 22:43:53 +0200

    main

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: "/geoinfo-lingua-web/", // <-- Add this line
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(
-    Boolean
-  ),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+export default defineConfig({
+  base: "/geoinfo-lingua-web/", // <-- This must match your repo name
+  plugins: [react()],
+});
